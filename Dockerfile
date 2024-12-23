1f0962d8581fdfe586cf399b7238c17e36664bb9
Michael Oberdorf <info@oberdorf-itc.de>
2024-12-23 18:16:21 +0100

    Upgrade dependencies

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
-FROM alpine:3.18.8 AS base
+FROM alpine:3.21.0 AS base
 RUN apk upgrade --available --no-cache --update \
     && apk add --no-cache --update \
-       python3=3.11.8-r1 \
-       py3-pip=23.1.2-r0 \
+       python3=3.12.8-r1 \
+       py3-pip=24.3.1-r0 \
     # Cleanup APK
     && rm -rf /var/cache/apk/* /tmp/* /var/tmp/*
 
@@ -11,8 +11,8 @@ RUN apk upgrade --available --no-cache --update \
 # Compiling python modules
 FROM base as builder
 RUN apk add --no-cache --update \
-      g++=12.2.1_git20220924-r10 \
-      python3-dev=3.11.8-r1 \
+      g++=14.2.0-r4 \
+      python3-dev=3.12.8-r1 \
     && ln -s /usr/include/locale.h /usr/include/xlocale.h
 COPY --chown=root:root FloatToHex /FloatToHex
 
@@ -26,14 +26,14 @@ RUN python3 setup.py install
 # Building the docker image with already compiled modules
 FROM base
 LABEL maintainer="Michael Oberdorf IT-Consulting <info@oberdorf-itc.de>"
-LABEL site.local.program.version="1.0.15"
+LABEL site.local.program.version="1.0.16"
 
 COPY --from=builder /usr/lib/python3.11/site-packages /usr/lib/python3.11/site-packages
 
 RUN apk add --no-cache --update \
-      libstdc++=12.2.1_git20220924-r10 \
-      py3-wheel=0.40.0-r1 \
-      py3-pandas=1.5.3-r1
+      libstdc++=14.2.0-r4 \
+      py3-wheel=0.43.0-r0 \
+      py3-pandas=2.2.3-r0
 
 COPY --chown=root:root /src /
 
--- src/app/modbus_client.py
+++ src/app/modbus_client.py
@@ -5,26 +5,27 @@
 # Author: Michael Oberdorf IT-Consulting
 # Datum: 2020-05-20
 # Last modified by: Michael Oberdorf
-# Last modified at: 2023-11-08
+# Last modified at: 2024-12-23
 ###############################################################################
 """
-import sys
-import os
-from pymodbus.client.sync import ModbusTcpClient as ModbusClient
-import logging
 import argparse
-import struct
-import pandas as pd
-import FloatToHex
-from numpy import little_endian
+import logging
+import sys
 
-VERSION='1.0.15'
-DEBUG=False
+import pandas as pd
+from pymodbus.client.sync import ModbusTcpClient as ModbusClient
+
+import FloatToHex
+
+VERSION = "1.0.16"
+DEBUG = False
 """
 ###############################################################################
 # F U N C T I O N S
 ###############################################################################
 """
+
+
 def parse_modbus_result(registers: list, startRegister: int, readLength: int, valueType: str, big_endian: bool = False):
     """
     parse_modbus_result - function to parse the modbus result and encode several format types
@@ -35,58 +36,61 @@ def parse_modbus_result(registers: list, startRegister: int, readLength: int, va
     @param big_endian: boolean, use big endian when calculating 32 bit values (default: False)
     @return: pandas.DataFrame(), table of calculated values per register
     """
-    previousRegister32 = '0000'
+    previousRegister32 = "0000"
     DATA = list()
     for register in registers:
         DATASET = dict()
-        DATASET['register'] = startRegister
-        if valueType == 'word':
-          htext = '{:04x}'.format(register, 'x')
-          DATASET['INT16'] = register
-          DATASET['UINT16'] = register & 0xffff
-          DATASET['HEX16'] = '0x' + htext.upper()
-          #decParts = [int(htext[i:i+2],16) for i in range(0,len(htext),2)]
-          #chrParts = [chr(val) for val in decParts]
-          #DATASET['ASCII'] = ' '.join(chrParts)
-          bitString = bin(int(htext, 16))[2:].zfill(16)
-          DATASET['BIT'] = bitString
+        DATASET["register"] = startRegister
+        if valueType == "word":
+            htext = "{:04x}".format(
+                register,
+            )
+            DATASET["INT16"] = register
+            DATASET["UINT16"] = register & 0xFFFF
+            DATASET["HEX16"] = "0x" + htext.upper()
+            # decParts = [int(htext[i:i+2],16) for i in range(0,len(htext),2)]
+            # chrParts = [chr(val) for val in decParts]
+            # DATASET['ASCII'] = ' '.join(chrParts)
+            bitString = bin(int(htext, 16))[2:].zfill(16)
+            DATASET["BIT"] = bitString
 
-          if big_endian: htext32 = previousRegister32 + htext
-          else: htext32 = htext + previousRegister32
+            if big_endian:
+                htext32 = previousRegister32 + htext
+            else:
+                htext32 = htext + previousRegister32
 
-          DATASET['HEX32'] = '0x' + (htext32).upper()
-          DATASET['INT32'] =  int(htext32, 16)
-          DATASET['UINT32'] =  DATASET['INT32'] & 0xffffffff
-          DATASET['FLOAT32'] = FloatToHex.hextofloat(DATASET['INT32'])
-          previousRegister32 = htext
+            DATASET["HEX32"] = "0x" + (htext32).upper()
+            DATASET["INT32"] = int(htext32, 16)
+            DATASET["UINT32"] = DATASET["INT32"] & 0xFFFFFFFF
+            DATASET["FLOAT32"] = FloatToHex.hextofloat(DATASET["INT32"])
+            previousRegister32 = htext
         else:
-          DATASET['BOOL'] = register
-          if register:
-            DATASET['BIT'] = 1
-          else:
-            DATASET['BIT'] = 0
+            DATASET["BOOL"] = register
+            if register:
+                DATASET["BIT"] = 1
+            else:
+                DATASET["BIT"] = 0
 
-        startRegister+=1
+        startRegister += 1
         DATA.append(DATASET)
 
         # break the loop if we reached the readLength
         if len(DATA) >= readLength:
-          break
-
+            break
 
     # Building data frame out of the dictionary
-    df = pd.DataFrame.from_dict(DATA, orient='columns')
-    df.set_index('register', drop=True, inplace=True)
+    df = pd.DataFrame.from_dict(DATA, orient="columns")
+    df.set_index("register", drop=True, inplace=True)
 
     # some conversions
-    if valueType == 'word':
-      df['INT16'] = df['INT16'].astype('int16')
-      df['UINT16'] = df['UINT16'].astype('uint16')
-      df['FLOAT32'] = df['FLOAT32'].fillna(0.0).astype('float')
-      df['INT32'] = df['INT32'].fillna(df['INT16']).astype('int32')
-      df['UINT32'] = df['UINT32'].fillna(df['UINT16']).astype('uint32')
+    if valueType == "word":
+        df["INT16"] = df["INT16"].astype("int16")
+        df["UINT16"] = df["UINT16"].astype("uint16")
+        df["FLOAT32"] = df["FLOAT32"].fillna(0.0).astype("float")
+        df["INT32"] = df["INT32"].fillna(df["INT16"]).astype("int32")
+        df["UINT32"] = df["UINT32"].fillna(df["UINT16"]).astype("uint32")
 
-    return(df)
+    return df
 
 
 """
@@ -96,69 +100,99 @@ def parse_modbus_result(registers: list, startRegister: int, readLength: int, va
 """
 
 # Initialize logger
-#FORMAT = ('%(asctime)-15s %(threadName)-15s  %(levelname)-8s %(module)-15s:%(lineno)-8s %(message)s')
-FORMAT = ('%(message)s')
+# FORMAT = ('%(asctime)-15s %(threadName)-15s  %(levelname)-8s %(module)-15s:%(lineno)-8s %(message)s')
+FORMAT = "%(message)s"
 logging.basicConfig(format=FORMAT)
 log = logging.getLogger()
 log.setLevel(logging.INFO)
 
 # Parsing command line arguments
-parser = argparse.ArgumentParser(description='Modbus TCP Client v'+ VERSION)
+parser = argparse.ArgumentParser(description="Modbus TCP Client v" + VERSION)
 group = parser.add_argument_group()
-group.add_argument('-s', '--slave', help='Hostname or IP address of the Modbus TCP slave (default: 127.0.0.1)', default='127.0.0.1')
-group.add_argument('-p', '--port', help='TCP port (default: 502)', default=502)
-group.add_argument('-i', '--slaveid', help='The slave ID, between 1 and 247 (default: 1)', default=1)
-group.add_argument('-t', '--registerType', help='Register type 1 to 4 to read (1=Discrete Output Coils, 2=Discrete Input Contacts, 3=Analog Output Holding Register, 4=Analog Input Register) (default: 3)', default=3)
-group.add_argument('-r', '--register', help='The register address between 0 and 9999 (default: 0)', default=0)
-group.add_argument('-l', '--length', help='How many registers should be read, between 1 and 125 (default: 1)', default=1)
-group.add_argument('-b', '--bigEndian', help='Use big endian instead of little endian when calculating the 32bit values', action='store_true', default=False)
-group.add_argument('-c', '--csv', help='Output as CSV', action='store_true', default=False)
-group.add_argument('-d', '--debug', help='Enable debug output', action='store_true', default=False)
+group.add_argument(
+    "-s", "--slave", help="Hostname or IP address of the Modbus TCP slave (default: 127.0.0.1)", default="127.0.0.1"
+)
+group.add_argument("-p", "--port", help="TCP port (default: 502)", default=502)
+group.add_argument("-i", "--slaveid", help="The slave ID, between 1 and 247 (default: 1)", default=1)
+group.add_argument(
+    "-t",
+    "--registerType",
+    help="Register type 1 to 4 to read (1=Discrete Output Coils, 2=Discrete Input Contacts, 3=Analog Output Holding Register, 4=Analog Input Register) (default: 3)",
+    default=3,
+)
+group.add_argument("-r", "--register", help="The register address between 0 and 9999 (default: 0)", default=0)
+group.add_argument(
+    "-l", "--length", help="How many registers should be read, between 1 and 125 (default: 1)", default=1
+)
+group.add_argument(
+    "-b",
+    "--bigEndian",
+    help="Use big endian instead of little endian when calculating the 32bit values",
+    action="store_true",
+    default=False,
+)
+group.add_argument("-c", "--csv", help="Output as CSV", action="store_true", default=False)
+group.add_argument("-d", "--debug", help="Enable debug output", action="store_true", default=False)
 args = parser.parse_args()
 # validate input
-if not isinstance(args.slaveid, int): args.slaveid = int(args.slaveid)
+if not isinstance(args.slaveid, int):
+    args.slaveid = int(args.slaveid)
 if args.slaveid < 1 or args.slaveid > 247:
-    log.error('SlaveID needs to be in a range between 1 and 247!')
+    log.error("SlaveID needs to be in a range between 1 and 247!")
     sys.exit(1)
-if not isinstance(args.registerType, int): args.registerType = int(args.registerType)
+if not isinstance(args.registerType, int):
+    args.registerType = int(args.registerType)
 if args.registerType < 1 or args.registerType > 4:
-    log.error('Register typer needs to be 1, 2, 3 or 4!')
+    log.error("Register typer needs to be 1, 2, 3 or 4!")
     sys.exit(1)
-if not isinstance(args.register, int): args.register = int(args.register)
+if not isinstance(args.register, int):
+    args.register = int(args.register)
 if args.register < 0 or args.register > 9999:
-    log.error('Register needs to be in a range between 0 and 9999!')
+    log.error("Register needs to be in a range between 0 and 9999!")
     sys.exit(1)
-if not isinstance(args.length, int): args.length = int(args.length)
+if not isinstance(args.length, int):
+    args.length = int(args.length)
 if args.length < 1 or args.length > 125:
-    log.error('Register read length needs to be in a range between 1 and 125!')
+    log.error("Register read length needs to be in a range between 1 and 125!")
     sys.exit(1)
 if args.length < 1 or args.register + args.length - 1 > 9999:
-    log.error('Register length does not exceed the maximum register addresses in a range between 1 and 9999!')
+    log.error("Register length does not exceed the maximum register addresses in a range between 1 and 9999!")
     sys.exit(1)
-if args.debug: DEBUG=True
+if args.debug:
+    DEBUG = True
 
 # change loglevel to DEBUG
-if DEBUG: log.setLevel(logging.DEBUG)
+if DEBUG:
+    log.setLevel(logging.DEBUG)
 
 # define some values, based on the input
 if args.registerType == 1:
-    register_type = 'Discrete Output Coils'
+    register_type = "Discrete Output Coils"
     register_number = args.register
 elif args.registerType == 2:
-    register_type = 'Discrete Input Contacts'
+    register_type = "Discrete Input Contacts"
     register_number = 10000 + args.register
 elif args.registerType == 3:
-    register_type = 'Analog Output Holding Register'
+    register_type = "Analog Output Holding Register"
     register_number = 40000 + args.register
 elif args.registerType == 4:
-    register_type = 'Analog Input Register'
+    register_type = "Analog Input Register"
     register_number = 30000 + args.register
 
 # start the master and connect to slave
 if DEBUG:
-    print('Starting Modbus TCP master client, v' + str(VERSION))
-    print('Trying to connect to slave: ' + str(args.slave) + ':' + str(args.port))
-    print('Query with slave id: ' + str(args.slaveid) + ', register: "' + register_type + '", start register: ' + str(args.register) + ', read register Length: ' + str(args.length))
+    print("Starting Modbus TCP master client, v" + str(VERSION))
+    print("Trying to connect to slave: " + str(args.slave) + ":" + str(args.port))
+    print(
+        "Query with slave id: "
+        + str(args.slaveid)
+        + ', register: "'
+        + register_type
+        + '", start register: '
+        + str(args.register)
+        + ", read register Length: "
+        + str(args.length)
+    )
     print()
 
 # create the client
@@ -166,7 +200,7 @@ client = ModbusClient(args.slave, port=args.port)
 # connect to server
 result = client.connect()
 if not result:
-    log.error('Error while connecting to Modbus TCP slave on:' + str(args.slave) + ':' + str(args.port))
+    log.error("Error while connecting to Modbus TCP slave on:" + str(args.slave) + ":" + str(args.port))
     sys.exit(2)
 
 
@@ -176,35 +210,48 @@ if not result:
 
 # read the registers, dependent on the requested type
 if args.registerType == 1:
-  rr = client.read_coils(args.register, args.length, unit=args.slaveid)
+    rr = client.read_coils(args.register, args.length, unit=args.slaveid)
 elif args.registerType == 2:
-  rr = client.read_discrete_inputs(args.register, args.length, unit=args.slaveid)
+    rr = client.read_discrete_inputs(args.register, args.length, unit=args.slaveid)
 elif args.registerType == 3:
-  rr = client.read_holding_registers(args.register, args.length, unit=args.slaveid)
+    rr = client.read_holding_registers(args.register, args.length, unit=args.slaveid)
 elif args.registerType == 4:
-  rr = client.read_input_registers(args.register, args.length, unit=args.slaveid)
+    rr = client.read_input_registers(args.register, args.length, unit=args.slaveid)
 if rr.isError():
-  log.error('Error while querying Modbus TCP slave!')
-  client.close()
-  sys.exit(3)
+    log.error("Error while querying Modbus TCP slave!")
+    client.close()
+    sys.exit(3)
 # close connection
 client.close()
 
 # parse the results
 if args.registerType >= 3:
-  df = parse_modbus_result(registers=rr.registers, startRegister=register_number, readLength=args.length, valueType='word', big_endian=args.bigEndian)
-  # sort and filter output
-  df = df[['HEX16', 'UINT16', 'INT16', 'BIT', 'HEX32', 'FLOAT32']] #, 'UINT32', 'INT32']]
+    df = parse_modbus_result(
+        registers=rr.registers,
+        startRegister=register_number,
+        readLength=args.length,
+        valueType="word",
+        big_endian=args.bigEndian,
+    )
+    # sort and filter output
+    df = df[["HEX16", "UINT16", "INT16", "BIT", "HEX32", "FLOAT32"]]  # , 'UINT32', 'INT32']]
 else:
-  df = parse_modbus_result(registers=rr.bits, startRegister=register_number, readLength=args.length, valueType='boolean', big_endian=args.bigEndian)
-  df = df[['BIT', 'BOOL']]
-
+    df = parse_modbus_result(
+        registers=rr.bits,
+        startRegister=register_number,
+        readLength=args.length,
+        valueType="boolean",
+        big_endian=args.bigEndian,
+    )
+    df = df[["BIT", "BOOL"]]
 
 
 # output results
-with pd.option_context('display.max_rows', None, 'display.max_columns', None, 'display.float_format', lambda x: '%.6f' % x):
+with pd.option_context(
+    "display.max_rows", None, "display.max_columns", None, "display.float_format", lambda x: "%.6f" % x
+):
     if args.csv:
-        df.to_csv(sys.stdout, sep=';')
+        df.to_csv(sys.stdout, sep=";")
     else:
         print(df)
 
